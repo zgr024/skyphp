cb2315eb1381e721d98bea36aa7a2b4d88539f6f
Zachary Rosenberg <zgr024@gmail.com>
2014-11-25 11:32:55 -0500

    5

--- templates/html5/html5.php
+++ templates/html5/html5.php
@@ -108,14 +108,30 @@ if ( $template_area == 'top' ) {
 	}
     if ( $this->google_analytics_account ) {
 		if ($_GET['debug_gaa']) echo $this->google_analytics_account;
+/*
 ?>
 <script>
-    var _gaq=[['_setAccount','<?=$this->google_analytics_account?>'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+   	var _gaq=[['_setAccount','<?=$this->google_analytics_account?>'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
 <?php
+*/
+
+?>
+
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	ga('create', '<?=$this->google_analytics_account?>', 'auto');
+	ga('require', 'displayfeatures');
+	ga('send', 'pageview');
+</script>
+<?
     }//google analytics
 
     global $db, $dbw, $db_host, $dbw_host;
