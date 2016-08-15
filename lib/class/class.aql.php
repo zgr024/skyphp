2c426efeab14bdfd4aee0422eb2111fcd3c16e3f
HarleyHWC <harley.fischel@hotwiremail.com>
2016-08-15 15:48:59 -0400

    new enhanced aql with trim and getRecords method

--- lib/class/class.aql.php
+++ lib/class/class.aql.php
@@ -1073,7 +1073,9 @@ class aql
         $rs = array();
         $microtime_start = microtime(true);
         $r = $db_conn->Execute($arr[$select_type]);
-		hwc_debug::add_aql($arr[$select_type], number_format(microtime(true) - $microtime_start,3));
+        if(class_exists('hwc_debug')){
+            hwc_debug::add_aql($arr[$select_type], number_format(microtime(true) - $microtime_start,3));
+        }
 
         if ($r === false) {
 
@@ -1148,15 +1150,15 @@ class aql
 
                         if ($query) {
                     // new getRecords method
-/*                          $ca = $s['constructor argument']; $p = new $m();
+                            $ca = $s['constructor argument']; $p = new $m();
                             $arr = array('ids'=>array_map(function($a) use ($ca) { return $a[$ca]; },$query));
-                            foreach($p->getRecords($arr) as $row){ $tmp[$k][]['_data'] = $row; }*/
+                            foreach($p->getRecords($arr) as $row){ $tmp[$k][]['_data'] = $row; }
                   // old query loop method
-                            foreach ($query as $row) {
+/*                            foreach ($query as $row) {
                                 $arg = $row[$s['constructor argument']];
                                 $o = Model::get($m, $arg, $sub_do_set);
                                 $tmp[$k][] = ($object) ? $o : $o->dataToArray();
-                            }
+                            }*/
                         }
                     } else if (!$s['plural']) {
                         $arg = (int) $tmp[$s['constructor argument']];
